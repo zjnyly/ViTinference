f7f92ae2b77ec150a8ceda1f2969f55dabe4e87f
Jianing Zheng <zjnylyapple@outlook.com>
2022-08-13 04:57:40 +0000

    version 2

---
+++ README.md
@@ -0,0 +1,26 @@
+# Extremely Slow ViT Inference Framework
+
+
+
+## Highlight:
+
+* Full function implementation, exactly like`torch` (`numpy`) **tensor** operations
+* Operation broadcast
+* Pure C implementation without any external libs
+
+
+
+## RUN:
+
+```
+sh ./run.sh
+```
+
+
+
+## To Do:
+
+* OOP abstraction
+* Better data layout
+* Better memory management
+
--- inference.cpp
+++ inference.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 #include "./layer/vit.cpp"
 
 int main()
@@ -6,7 +7,11 @@ int main()
     // Fake input image, just one image per batch
     std::vector<int> inputDataDimension = {1, 3, 224, 224};
     auto inputData = new Tensor<double>(inputDataDimension, true);
+
+    //////////////////////////////////
     auto ans = ViT<double>(inputData);
+    //////////////////////////////////
+
     std::cout<<"猫  狗"<<std::endl;
     ans->showRawData();
     return 0;
